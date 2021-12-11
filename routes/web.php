b3c4be4652cce1d101082bd0313ec776adc7aae1
Lele962 <ysteamlqszl2@163.com>
2021-12-11 15:44:54 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
