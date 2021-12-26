4279982ff0a710f5a67f3530684a21062f315971
Lele962 <ysteamlqszl2@163.com>
2021-12-26 17:55:14 +0800

    第二次

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
 class User extends Authenticatable
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -18,13 +18,13 @@
           <label for="email" class="col-md-4 col-form-label text-md-right">Email 地址</label>
 
           <div class="col-md-6">
-            <input id="email" type="email" class="form-control{}" required autofocus>
+            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
 
-            
+            @if ($errors->has('email'))
             <span class="invalid-feedback" role="alert">
-              <strong>{}</strong>
+              <strong>{{ $errors->first('email') }}</strong>
             </span>
-            
+            @endif
           </div>
         </div>
 
@@ -32,13 +32,13 @@
           <label for="password" class="col-md-4 col-form-label text-md-right">密码</label>
 
           <div class="col-md-6">
-            <input id="password" type="password" class="form-control{}" name="password" required>
+            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-            
+            @if ($errors->has('password'))
             <span class="invalid-feedback" role="alert">
-              <strong>{}</strong>
+              <strong>{{ $errors->first('password') }}</strong>
             </span>
-            
+            @endif
           </div>
         </div>
 
